0b6526974dba7797a27f84beb4a45081557fbbcf
bho <bho.com@gmail.com>
2013-02-13 11:41:49 -0700

    drop db on start, eeasy way to clean stale data

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var express = require('express')
 	, path = require('path')
 	, request = require('request')
 	, underscore = require('underscore')._
-	, cons = require("consolidate");
+	, cons = require("consolidate")
+	, mongoose = require('mongoose')
+;
 
 var app = express();
 
@@ -38,5 +40,21 @@ var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-// init Josie
-var josieApp = require('./lib/JosieApp')(app, server, "http://ci.attask.com/jenkins/");
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/josie', function (err){
+
+
+	mongoose.connections[0].db.dropDatabase(function (){
+		console.log("DROP", arguments);
+		start();
+	});
+
+});
+
+
+
+function start (){
+	// init Josie
+	var josieApp = require('./lib/JosieApp')(app, server, "http://ci.attask.com/jenkins/");
+}
+
